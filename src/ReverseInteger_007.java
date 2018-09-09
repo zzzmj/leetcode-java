86478e848cb6cc25b90720041d3a245abc73a580
zmj <18270632198@163.com>
2018-09-09 17:41:47 +0800

    Solution007

--- src/ReverseInteger_007.java
+++ src/ReverseInteger_007.java
@@ -1,9 +1,25 @@
+
 /**
  * @author 1
- * @Description
+ * @Description 将一个整数反转输出
  * @create 2018/9/9
  * @since 1.0.0
  */
 public class ReverseInteger_007 {
+  
+  public int reverses(int x) {
+    int num = 0;
+    while(x != 0) {
+      if(num > (Integer.MAX_VALUE)/10 - (x%10)/10) {
+        return 0;
+      }
+      if(num < (Integer.MIN_VALUE)/10 - (x%10)/10) {
+        return 0;
+      }
+      num = num*10 + x%10;
+      x /= 10;
+    }
+    return num;
+  }
   
 }
